6929a87f8f859d5b0cbf1b5c5a1c0d7958297315
RitikaSharma003 <ritikasharma20032003@gmail.com>
2025-07-07 09:14:57 +0530

    upload resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,15 +7,18 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}
-        style={{marginTop:'20px'}}>Hi, I'm Ritika Sharma</h1>
-        <p className={styles.description}>A Passionate Web Developer and Software Developer.
-     
+        <h1 className={styles.title} style={{ marginTop: "20px" }}>
+          Hi, I'm Ritika Sharma
+        </h1>
+        <p className={styles.description}>
+          A Passionate Web Developer and Software Developer.
         </p>
-        <a href="/public/Resume Ritika Sharma.pdf" 
-        download 
-        className={styles.contactBtn} >
-          Download CV 
+        <a
+          href="/public/Resume Ritika Sharma.pdf"
+          download
+          className={styles.contactBtn}
+        >
+          Download CV
         </a>
       </div>
       <img
